245a7e6e4fdc8b8b1bddf5115ee9ef6300ab4c92
tilak9606 <tilakgajjar9606@gmail.com>
2025-11-04 12:04:30 +0530

    unncecessary comments removed

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,20 +8,6 @@ dotenv.config();
 
 const PORT = process.env.PORT || 8000;
 
-// const allowedOrigins = [
-//   /http:\/\/(localhost|127\.0\.0\.1)(:\\d+)?$/,
-// ];
-
-// const corsOptions = {
-//   origin: (origin, callback) => {
-//     // if (!origin) return callback(null, true); 
-//     // const ok = allowedOrigins.some((o) => (o instanceof RegExp ? o.test(origin) : o === origin));
-//     // callback(ok ? null : new Error("Not allowed by CORS"), ok);
-//   },
-//   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-//   credentials: true,
-// };
-
 app.use(cors());
 app.use(express.json());
 
